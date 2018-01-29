6a41cbb63475e4d8460b56dbd32ecfaa65fd9e9b
zjj <815145569@qq.com>
2018-01-29 19:12:19 +0800

    1

--- GoRedisLock/redisLock.go
+++ GoRedisLock/redisLock.go
@@ -4,6 +4,44 @@ date: 2018/1/28
 */
 package GoRedisLock
 
+import (
+	"github.com/go-redis/redis"
+	"github.com/bsm/redis-lock"
+	"fmt"
+	"time"
+)
+
 // redis-lock 简单的使用例子
+func GetRedisLock() {
 
+	// redis 客户端
+	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", Password: "", DB: 1})
+	key := "testRedisLock"
+	// 新建锁
+	l := lock.New(client, key, nil)
+	// 申请锁 默认5s
+	l.Lock()
 
+	// 别的Locker拿不到相同key的锁
+	l2 := lock.New(client, key, nil)
+	fmt.Println(l2.Lock()) // false <nil>
+
+	// 自己可以刷新
+	fmt.Println(l.Lock()) // true <nil>
+
+	// 释放后别的Locker可以拿到锁
+	l.Unlock()
+	l3, _ := lock.Obtain(client, key, nil)
+	fmt.Println("Locker", l3)
+
+	// 等待锁自动释放
+	time.Sleep(time.Second * 6)
+	lock.Run(client, key, nil, run)
+
+}
+
+func run() error {
+	fmt.Println("GetLock")
+
+	return nil
+}
--- GoRedisLock/redisLock_test.go
+++ GoRedisLock/redisLock_test.go
@@ -5,22 +5,9 @@ date: 2018/1/28
 package GoRedisLock
 
 import (
-	"fmt"
 	"testing"
-	"time"
-
-	"github.com/go-redis/redis"
-
-	"github.com/bsm/redis-lock"
 )
 
 func TestRedisLock(t *testing.T) {
-	client := redis.NewClient(&redis.Options{Addr: "127.0.0.1:6379", Password: "", DB: 1})
-	l := lock.New(client, "testRedisLock", nil)
-	fmt.Println(l)
-	fmt.Println(l.Lock())
-
-	time.Sleep(time.Second * 10)
-
-	fmt.Println(l.IsLocked())
+	GetRedisLock()
 }
